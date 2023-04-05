f044fbd6b28ac36e4339000d491e9a1dedc25e3e
leewang0207 <iwanggeun009@gmail.com>
2023-04-05 19:27:56 +0900

    00

---
+++ Ch01/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h3>Ch01.Vue 소개</h3>
+  <p>Hello World</p>
+</template>
+
+<script>
+/**
+ * 날짜 : 2023/04/05
+ * 이름 : 이왕근
+ * 내용 : Ch01.Vue.js 소개
+ *
+ * 개발환경 설정
+ *  - node.js 설치
+ *  - VSCode 설치 및 확장 플러그인(volar, vue3 snippets, prettier, ESLint)
+ *  - Vue.js devtools Chrome 확장 플러그인 설치
+ *
+ * 프로젝트 생성/실행
+ *  - 프로젝트 생성 : npm init vite-app Ch01
+ *  - 프로젝트 이동 : cd Ch01
+ *  - 라이브러리 설치 : npm install
+ *  - 프로젝트 실행 : npm run dev
+ *  - 프로젝트 배포 : npm run build
+ */
+export default {
+  name: "App",
+};
+</script>
