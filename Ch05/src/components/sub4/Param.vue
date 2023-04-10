df14de683852fd7e940a65d8cade9f98a1368a9e
leewang0207 <iwanggeun009@gmail.com>
2023-04-10 17:25:39 +0900

    2023/04/10

---
+++ Ch05/src/components/sub4/Param.vue
@@ -0,0 +1,20 @@
+<template>
+    <h4>Param</h4>
+    <p>
+        user : {{ user }}<br/>
+        uid: {{ user.uid }}<br/>
+        name: {{ user.name }}<br/>
+        age: {{ user.age }}<br/>
+
+    </p>
+</template>
+<script setup>
+import { useRoute } from "vue-router";
+
+// route 생성
+const route = useRoute();
+
+// params 데이터 수신
+const user = route.params;
+</script>
+<style scoped></style>
